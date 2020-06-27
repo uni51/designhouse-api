b3ff5b39f8c84f5dabeeedad6ee1d1a798da7533
uni51 <uni51@users.noreply.github.com>
2020-06-27 16:14:55 +0900

    Section3：14. Email Veritification 1: Setting up the user activation process

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -2,32 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\VerifiesEmails;
+//use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Email Verification Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling email verification for any
-    | user that recently registered with the application. Emails may also
-    | be re-sent if the user didn't receive the original email message.
-    |
-    */
-
-    use VerifiesEmails;
-
-    /**
-     * Where to redirect users after verification.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
     /**
      * Create a new controller instance.
      *
@@ -35,8 +18,36 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+
+    public function verify(Request $request, User $user)
+    {
+        // check if the url is a valid signed url
+        if(! URL::hasValidSignature($request)){
+            return response()->json(["errors" => [
+                "messgae" => "Invalid verification link"
+            ]], 422); // Laravelの場合、バリデーションエラーは422
+        }
+
+        // check if the user has already verified account
+        if($user->hasVerifiedEmail()){
+            return response()->json(["errors" => [
+                "messgae" => "Email address already verified"
+            ]], 422);
+        }
+
+        $user->markEmailAsVerified();
+        event(new Verified($user));
+
+        return response()->json(['message' => 'Email successfully verified'], 200);
+    }
+
+
+    public function resend(Request $request)
+    {
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,8 @@ Route::group(['middleware' => ['auth:api']], function(){
 
 
 // Route group for guests only
-
 Route::group(['middleware' => ['guest:api']], function(){
     Route::post('register', 'Auth\RegisterController@register');
+    Route::post('verification/verify', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('verification/resend', 'Auth\VerificationController@resend');
 });
